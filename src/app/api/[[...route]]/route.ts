39d118855eaae0a1e3d7bdcecbdd10c4d6842aca
itachi <94786908+doma-itachi@users.noreply.github.com>
2024-12-31 01:49:11 +0900

    APIルートを実装 RPCを追加

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,15 +1,32 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { getPopulationComposition, getPrefectures } from "../lib/resas";
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
 
 export const runtime = "nodejs";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c)=>{
-    return c.json({
-        message: "Hello Next.js!"
-    })
-})
+app.get("/prefectures", async (c) => {
+    return c.json(await getPrefectures());
+});
+
+app.get(
+    "/population",
+    zValidator(
+        "query",
+        z.object({
+            prefCode: z.string()
+        })
+    ),
+    async(c)=>{
+        const query = await c.req.valid("query");
+        return c.json(await getPopulationComposition(parseInt(query.prefCode)));
+    }
+);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof app;
