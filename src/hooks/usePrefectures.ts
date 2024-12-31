49b8a29f6823b2be964703235883a71d301a6cbf
itachi <94786908+doma-itachi@users.noreply.github.com>
2024-12-31 12:44:50 +0900

    usePrefecturesフックを実装

---
+++ src/hooks/usePrefectures.ts
@@ -0,0 +1,25 @@
+import type { Prefecture } from "@/app/types";
+import { client } from "@/lib/hono";
+import { useEffect, useMemo, useState } from "react";
+
+export function usePrefectures() {
+    const [prefectures, setPrefectures] = useState<Prefecture[]>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<Error>();
+
+    useEffect(() => {
+        (async () => {
+            setIsLoading(true);
+            try {
+                const res = await client.api.prefectures.$get();
+                setPrefectures(await res.json());
+            } catch (e: any) {
+                setError(e);
+            } finally {
+                setIsLoading(false);
+            }
+        })();
+    }, []);
+
+    return { prefectures, isLoading, error };
+}
