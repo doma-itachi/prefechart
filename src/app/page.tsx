773b059e31bd63323b5fa2ce3320d5e867c31746
doma-itachi <94786908+doma-itachi@users.noreply.github.com>
2025-01-11 00:02:23 +0900

    ビルド時にfetchに失敗する問題を修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 import { client } from "@/lib/hono";
 import { Client } from "./client";
 import type { Prefecture } from "./types";
+import { getPrefectures } from "./api/lib/resas";
 
 export default async function Home() {
-    const res = await client.api.prefectures.$get();
-    if (!res.ok) {
-        throw new Error("都道府県一覧の取得に失敗");
+    let prefectures: Prefecture[];
+    try{
+        const res = await (await client.api.prefectures.$get()).json();
+        prefectures = res;
     }
-    const prefectures: Prefecture[] = await res.json();
+    catch{
+        try{
+            const res = await getPrefectures();
+            prefectures = res;
+        }
+        catch{
+            throw new Error("都道府県一覧の取得に失敗");
+        }
+    }
 
     return (
         <div className="md:mx-6">
